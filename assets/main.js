7c6060f4749ec4eeb979b97eb62c2baa597dc28f
Alek90 <4lek.90@gmail.com>
2021-10-25 16:12:47 +0200

    Creata la struttura che vorremmo riprodurre.
    Dato sostanza alla struttura precedentemente commentata in html.

--- assets/main.js
+++ assets/main.js
@@ -8,21 +8,43 @@
 
 //  Array di oggetti per post:
 
-const post = [
+const posts = [
     {
         identity: "",
         content: "",
         image: "",
-        writer: "",
-        likes: "",
+        writer: "Da'thral",
+        imgAvatar: "https://lodisseadelmaster.altervista.org/wp-content/uploads/2016/10/classe-di-prestigio-nano-dd-3.5-500x375.png",
+        likes: 86,
         creationDate: ""
     },
     {
         identity: "",
         content: "",
         image: "",
-        writer: "",
-        likes: "",
+        writer: "Dak'dros",
+        likes: 120,
+        creationDate: ""
+    },
+    {
+        identity: "",
+        content: "",
+        image: "",
+        writer: "Du-bur",
+        likes: 97,
         creationDate: ""
     }
 ]
+
+
+for (i=0; i<posts.length; i++) {
+
+    const post = posts[i];
+    console.log(post);
+
+    const postPrinted = `
+        
+    
+    
+    `
+}

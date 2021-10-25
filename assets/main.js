01e27557f1c4d2964aec36429b72dde0ad6ac6d6
Alek90 <4lek.90@gmail.com>
2021-10-25 18:27:19 +0200

    Aggiunto css.

--- assets/main.js
+++ assets/main.js
@@ -11,28 +11,28 @@
 const posts = [
     {
         identity: "",
-        content: "",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quas, beatae delectus illum non natus modi doloremque fugiat, quibusdam laborum eveniet porro a placeat blanditiis nihil facilis repudiandae possimus suscipit, commodi alias amet earum! Doloremque.",
         image: "https://i.picsum.photos/id/1022/367/267.jpg?hmac=0mbNHdQLUp_d5Cat6xNrL06S_07ikrdfLb6FDKJuir4",
         writer: "Da'thral",
         imgAvatar: "https://lodisseadelmaster.altervista.org/wp-content/uploads/2016/10/classe-di-prestigio-nano-dd-3.5-500x375.png",
         likes: 86,
-        creationDate: ""
+        creationDate: "today"
     },
     {
         identity: "",
-        content: "",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ullam voluptate maiores inventore nobis animi suscipit magni, quis eveniet corporis!",
         image: "https://i.picsum.photos/id/1003/367/267.jpg?hmac=1VHS13exKZ5QWJdtDTu0iCUTBZKCE_clSpR-wc3PW3g",
         writer: "Dak'dros",
         likes: 120,
-        creationDate: ""
+        creationDate: "today"
     },
     {
         identity: "",
-        content: "",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod placeat dignissimos voluptates consectetur soluta, alias repellat! Aperiam fuga non adipisci ex sunt quos, nisi in amet cumque qui officiis dolores labore rem ea unde excepturi dignissimos omnis, exercitationem tempore, dolorem eveniet? Voluptates veniam omnis error.",
         image: "https://i.picsum.photos/id/1015/367/267.jpg?hmac=mjgo8WXn4uxkZ_GhSDNN7rUSzP9jgyPBsHrDF02oU-o",
         writer: "Du-bur",
         likes: 97,
-        creationDate: ""
+        creationDate: "today"
     }
 ]
 
@@ -47,21 +47,21 @@ for (i=0; i<posts.length; i++) {
         <div class="post">
 
             <header class="post_header">
-                <img src="${post.imgAvatar}" alt="">
+                <img src=${post.imgAvatar} alt="">
                 <section class="published">
-                    <h4>"${post.writer}"</h4>
-                    <div>"${post.creationDate}"</div>
+                    <h4>${post.writer}</h4>
+                    <div>${post.creationDate}</div>
                 </section>
 
             </header>
 
             <main class="post_main">
-                <p>"${post.content}"</p>
+                <p>${post.content}</p>
                 <img src="${post.image}" alt="">
             </main>
 
             <footer class="post_footer">
-                <div class="add_like">""</div>
+                <div class="add_like">Mi Piace</div>
                 <div class="total_like">${post.likes} like</div>
             </footer>
 
